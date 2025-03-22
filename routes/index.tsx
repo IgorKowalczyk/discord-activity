b1ece007240410f783e3830b1a29fd170e83b71d
Igor Kowalczyk <majonezexe@protonmail.com>
2025-03-22 21:24:15 +0100

    Update dependencies in deno.lock and improve layout classes in index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -19,7 +19,7 @@ export default function Home(ctx: FreshContext) {
       </a>
      </div>
     </div>
-    <div className="items-center justify-end md:flex hidden lg:col-span-1">
+    <div className="lg:flex lg:items-center lg:justify-end lg:col-span-1 hidden">
      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="aspect-square rounded-full size-52 stroke-white ">
       <path d="M18.2 12.27 20 6H4l1.8 6.27a1 1 0 0 0 .95.73h10.5a1 1 0 0 0 .96-.73Z" />
       <path d="M8 13v9" />
